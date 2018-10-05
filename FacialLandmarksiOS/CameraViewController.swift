5e7933149565fd2a1b42f5370a6bbc49ecafd442
Khurram Shehzad <khurram.pak522@gmail.com>
2018-10-05 08:35:26 +0500

    Implement face detection

--- FacialLandmarksiOS/CameraViewController.swift
+++ FacialLandmarksiOS/CameraViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Example. All rights reserved.
 //
 
+import Vision
 import AVFoundation
 import UIKit
 
@@ -20,6 +21,8 @@ override func viewDidLoad() {
 }
     
 private let session = AVCaptureSession()
+private let faceDetectionRequest = VNDetectFaceRectanglesRequest()
+private let faceDetectionRequestHandler = VNSequenceRequestHandler()
 private var cameraView: CameraView {
     return view as! CameraView
 }
@@ -27,6 +30,33 @@ private var cameraView: CameraView {
 }
 
 extension CameraViewController {
+    
+private func handleDetection(detectionResults: [VNFaceObservation]) {
+    DispatchQueue.main.async {
+        if let sublayers = self.view.layer.sublayers {
+            for layer in sublayers[1...] {
+                layer.removeFromSuperlayer()
+            }
+        }
+        let viewWidth = self.view.frame.size.width
+        let viewHeight = self.view.frame.size.height
+        for result in detectionResults {
+            
+            let layer = self.newRectangularLayer()
+            
+            var rect = result.boundingBox
+            rect.origin.x *= viewWidth
+            rect.size.height *= viewHeight
+            rect.origin.y = ((1 - rect.origin.y) * viewHeight) - rect.size.height
+            rect.size.width *= viewWidth
+            
+            layer.frame = rect
+            layer.borderWidth = 2
+            layer.borderColor = UIColor.red.cgColor
+            self.view.layer.addSublayer(layer)
+        }
+    }
+}
 private func isCameraAuthorized() -> Bool {
     let authorizationStatus = AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
     switch authorizationStatus {
@@ -49,10 +79,11 @@ private func configureSession() {
     
     cameraView.session = session
     
-    let cameraDevices = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back)
+    let position = AVCaptureDevice.Position.front
+    let cameraDevices = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: position)
     var cameraDevice: AVCaptureDevice?
     for device in cameraDevices.devices {
-        if device.position == .back {
+        if device.position == position {
             cameraDevice = device
             break
         }
@@ -73,13 +104,36 @@ private func configureSession() {
     if session.canAddOutput(videoDataOutput) {
         session.addOutput(videoDataOutput)
     }
-    cameraView.videoPreviewLayer.videoGravity = .resize
+    cameraView.videoPreviewLayer.videoGravity = .resizeAspectFill
     session.startRunning()
+}
+private func newRectangularLayer() -> CALayer {
+    
+    let layer = CALayer()
+    layer.borderWidth = 2
+    layer.borderColor = UIColor.red.cgColor
+    return layer
 }
 }
 
 extension CameraViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
 // MARK: - Camera Delegate and Setup
 func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+    guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+        return
+    }
+    var imageRequestOptions = [VNImageOption: Any]()
+    if let cameraData = CMGetAttachment(sampleBuffer, key: kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix, attachmentModeOut: nil) {
+        imageRequestOptions[.cameraIntrinsics] = cameraData
+    }
+    do {
+        try faceDetectionRequestHandler.perform([faceDetectionRequest], on: pixelBuffer, orientation: CGImagePropertyOrientation(rawValue: 6)!)
+    }
+    catch {
+        print(error)
+        return
+    }
+    guard let results = faceDetectionRequest.results as? [VNFaceObservation] else { return }
+    handleDetection(detectionResults: results)
 }
 }
